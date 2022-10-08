96072540414173a14d5d75d782b2491c97413b99
Rahul Jha <63286808+rahuljha18101996@users.noreply.github.com>
2022-10-09 01:29:19 +0530

    Create ADLNode.java

---
+++ Graph/Representation/AdjacencyList/ADLNode.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public record ADLNode(int value, List<ADLNode> children) {
+    public boolean isAdjacent(ADLNode node) {
+        for (ADLNode n : node.children()) {
+            if (n == this) return true;
+        }
+        return false;
+    }
+}
