165647026a8a69d439f0f9f3209d0fe259636dc0
Rahul Jha <63286808+rahuljha18101996@users.noreply.github.com>
2022-10-09 01:33:21 +0530

    Create ADMNode.java

---
+++ Graph/Representation/AdjacencyMatrix/ADMNode.java
@@ -0,0 +1,9 @@
+import java.util.List;
+public record ADMNode(int value, List<ADMNode> children) {
+    public boolean isAdjacent(ADMNode node) {
+        for (ADMNode n : node.children()) {
+            if (n == this) return true;
+        }
+        return false;
+    }
+}
