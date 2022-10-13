d0e0e781d96ed1088de1dd06bd7c4f643d499b04
Rahul Jha <63286808+rahuljha18101996@users.noreply.github.com>
2022-10-13 06:51:47 +0530

    Create QuickSort.java
    Quick Sort

---
+++ Arrays/Sorting/QuickSort.java
@@ -0,0 +1,60 @@
+package Arrays.Sorting;
+
+public class QuickSort {
+    private static void divide(int[] a, int low,int high) {
+        if (low >= high) return;
+        int pivot = pivot(a,low,high);
+        divide(a,low,pivot - 1);
+        divide(a,pivot + 1,high);
+    }
+    private static int pivot(int[] a,int low,int high) {
+        int i = low - 1;
+        int pivotPoint = a[high];
+        for (int j = low; j < high; j++) {
+            if (i < high && pivotPoint >= a[j]) {
+                i++;
+                swap(a,i,j);
+            }
+        }
+        swap(a,i + 1,high);
+        return i + 1;
+    }
+    private static void swap(int[] a,int i,int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    private static void printArray(int[] a,char variableName) {
+        if (a.length == 0) {
+            System.out.printf("%c = []",variableName);
+            return;
+        }
+        System.out.printf("%c = [%d",variableName,a[0]);
+        for (int i = 1; i < a.length; i++) System.out.printf(",%d",a[i]);
+        System.out.printf("]%n");
+    }
+
+    public static void main(String[] args) {
+        int n = 1000;
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) array[i] = n--;
+        //printArray(array,'f');
+        long t1 = System.currentTimeMillis();
+        divide(array,0,array.length - 1);
+        t1 = System.currentTimeMillis() - t1;
+        //printArray(array,'f');
+        System.out.printf("Time taken to sort array is %d ms%n",t1);
+    }
+}
+
+/*
+Stats for Merge Sort
+
+    Input Size      Time(ms)
+    10               0
+    100              0
+    1,000            2
+    10,000           30
+    100,000          Program Crashed
+PC Specification: i9 12th Gen
+*/
